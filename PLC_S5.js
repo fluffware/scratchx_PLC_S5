1343f2bfdeff5a1a471e4d17733e50758d4477ec
Simon Berg <ksb@fluffware.se>
2015-08-08 20:58:28 +0200

    WIP

--- PLC_S5.js
+++ PLC_S5.js
@@ -1,3 +1,99 @@
+
+function PLC_S5_comm(url)
+{
+    var ws;
+    connect(url);
+    var self = this;
+
+    self.onDin = function(addr,reply) {};
+    self.onDout = function(addr,reply) {};
+    self.onAin = function(addr,reply) {};
+    
+    function connect(url)
+    {
+	ws = new WebSocket(url, "PLC_IO");
+	ws.onmessage = function(event) {
+	    var res = JSON.parse(event.data);
+	    switch(res.cmd) {
+	    case "din":
+		self.onDin(res.addr, res.reply);
+		break;
+	    case "dout":
+		self.onDout(res.addr, res.reply);
+		break;
+	    case "ain":
+		self.onAin(res.addr, res.reply);
+		break;
+	    }
+	}
+	ws.onError = onErrorHandler;
+	ws.onClose = onCloseHandler;
+    }
+    this.disconnect = function()
+    {
+	if (ws != undefined) {
+	    ws.close
+	    ws = undefined;
+	}
+    }
+
+    
+    function onErrorHandler(event)
+    {
+	
+    }
+    
+    function onCloseHandler(event)
+    {
+	if (event.code != CloseEvent.NORMAL) {
+	    
+	}
+    }
+
+    
+
+    
+    function send_cmd()
+    {
+	ws.send("{'cmd':'dout', 'addr':"+32+",'value':"+bits+"}");
+	bits++;
+    }
+
+    
+    function close_connection()
+    {
+	console.log('Closed');
+	ws.close();
+    }
+
+    this.dout = function(addr, bits, mask)
+    {
+	if (mask == undefined) {
+	    mask = 0xff;
+	}
+	ws.send("{'cmd':'dout', 'addr':"+addr+",'value':"
+		+bits+",'mask':"+mask+"}");	
+    }
+    this.din = function(addr)
+    {
+	ws.send("{'cmd':'din', 'addr':"+addr+"}");	
+    }
+    
+    this.ain = function(addr)
+    {
+	ws.send("{'cmd':'ain', 'addr':"+addr+"}");	
+    }
+    
+    this.aout = function(addr,value)
+    {
+	var iv = parseFloat(value) * 1000;
+	if (iv >= 10000) iv = 10000-1;
+	if (iv < 0) iv = 0;
+	ws.send("{'cmd':'aout', 'addr':"+addr+", 'value':"+iv+"}");	
+    }
+ 
+}
+
 (function(ext) {
     // Connect to I/O server
     var  io = new PLC_S5_comm('ws://localhost:28280');

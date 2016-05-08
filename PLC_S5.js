50ca27727ead0d3fb27470d897277a98fe266885
Simon Berg <ksb@fluffware.se>
2016-05-08 16:14:42 +0200

    Removed old communication code. Only stop discovery if a reply is received, not just an echoed request.

--- PLC_S5.js
+++ PLC_S5.js
@@ -1,104 +1,4 @@
 
-function PLC_S5_comm(url)
-{
-    var ws;
-    connect();
-    var self = this;
-
-    self.onDin = function(addr,reply) {};
-    self.onDout = function(addr,reply) {};
-    self.onAin = function(addr,reply) {};
-    
-    function connect()
-    {
-	ws = new WebSocket(url, "PLC_IO");
-	ws.onmessage = function(event) {
-	    var res = JSON.parse(event.data);
-	    switch(res.cmd) {
-	    case "din":
-		self.onDin(res.addr, res.value);
-		break;
-	    case "dout":
-		self.onDout(res.addr, res.value);
-		break;
-	    case "ain":
-		self.onAin(res.addr, res.value);
-		break;
-	    }
-	}
-	ws.onerror = onErrorHandler;
-	ws.onclose = onCloseHandler;
-    }
-    this.disconnect = function()
-    {
-	if (ws != undefined) {
-	    ws.close()
-	}
-    }
-
-    var reconnectTimer = undefined;
-    
-    function onErrorHandler(event)
-    {
-	
-    }
-    
-    function onCloseHandler(event)
-    {
-	if (event.code != CloseEvent.NORMAL) {
-	    
-	}
-	ws = undefined;
-	reconnectTimer = setTimeout(connect, 2000);
-    }
-
-    
-
-    
-    function send_cmd()
-    {
-	ws.send("{'cmd':'dout', 'addr':"+32+",'value':"+bits+"}");
-	bits++;
-    }
-
-    
-    function close_connection()
-    {
-	console.log('Closed');
-	ws.close();
-    }
-
-    this.connected = function() {
-	return ws != undefined && ws.readyState == WebSocket.OPEN;
-    }
-    
-    this.dout = function(addr, bits, mask)
-    {
-	if (mask == undefined) {
-	    mask = 0xff;
-	}
-	ws.send("{'cmd':'dout', 'addr':"+addr+",'value':"
-		+bits+",'mask':"+mask+"}");	
-    }
-    this.din = function(addr)
-    {
-	ws.send("{'cmd':'din', 'addr':"+addr+"}");	
-    }
-    
-    this.ain = function(addr)
-    {
-	ws.send("{'cmd':'ain', 'addr':"+addr+"}");	
-    }
-    
-    this.aout = function(addr,value)
-    {
-	var iv = parseFloat(value) * 1000;
-	if (iv >= 10000) iv = 10000-1;
-	if (iv < 0) iv = 0;
-	ws.send("{'cmd':'aout', 'addr':"+addr+", 'value':"+iv+"}");	
-    }
- 
-}
 
 function crc8_update(crc, data) {
     var crc_table = [
@@ -169,6 +69,7 @@ function crc8_update(crc, data) {
     ];
     return crc_table[crc ^ data];
 }
+
 function calculate_crc8(bytes)
 {
     var crc = 0xff;
@@ -327,7 +228,8 @@ function calculate_crc8(bytes)
 	if (l > replyBuffer.length) return;
 	var reply = replyBuffer.slice(0,l);
 	if (calculate_crc8(reply) == 0) {
-	    if (discoverTimeout != null) {
+	    // Stop discovery if we get a reply
+	    if (discoverTimeout != null && ((reply[0] & 0x20) != 0)) {
 		clearTimeout(discoverTimeout);
 		discoverTimeout = null;
 	    }
